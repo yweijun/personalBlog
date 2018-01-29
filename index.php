66baa8eab519ad7f31a2e33e9512033fe75cf9a3
yweijun <any_way@163.com>
2018-01-29 20:30:05 +0800

    1、跨域

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
 define('APP_PATH', __DIR__ . '/app/');
 
 require __DIR__ . '/thinkphp/base.php';// 加载框架基础文件
+header('Access-Control-Allow-Origin:*');//允许所有来源访问
+header('Access-Control-Allow-Method:POST,GET');//允许访问的方式
+header('Access-Control-Allow-Headers: X-PINGOTHER, Content-Type');
 
 // 加载框架引导文件
 think\App::run()->send();// 执行应用
